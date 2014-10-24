318b186813e3433ec460a000eee6addea9d6bc6d
hacone <esperhacone@gmail.com>
2014-10-24 14:16:54 +0900

    Readme and demo files

---
+++ download_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# download the demo and test files
+
+wget http://www.gi.k.u-tokyo.ac.jp/~hacone/Demo.tar.gz
--- install.sh
+++
@@ -1,3 +0,0 @@
-# make package for distribution
-bin/sbt package-dist
-sed -ie "s/utgenome.sample.Hello/hacone.AgIn.AgIn/" target/dist/bin/classworld.conf
